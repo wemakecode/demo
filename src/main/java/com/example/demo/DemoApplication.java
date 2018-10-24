2285cdcb55dd401254b8064c73ae29358ea7b976
HANPASS\055055 <055055@hanpass.com>
2018-10-24 10:07:38 +0900

    f

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,5 +13,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("Spring Boot Started.");!!@!@
+        System.out.println("Spring Boot Started.");
+        #!@#!@#!@
     }
 }
