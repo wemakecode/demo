97347f9b55e9e106306d52f8f3e9a0a10557aa4c
HANPASS\jongui <jongui@hanpass.com>
2018-10-24 11:38:05 +0900

    test001

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,15 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
-@ComponentScan
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("Spring Boot Started.");!!@!@
         System.out.println("Spring Boot Started.");
-        #!@#!@#!@
     }
 }
