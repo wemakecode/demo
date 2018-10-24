6df730c77b930f69a0fe378f9875b04c076fad25
HANPASS\055055 <055055@hanpass.com>
2018-10-24 13:39:00 +0900

    순정 파일 유지

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,4 +11,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("Spring Boot Started.");
-}
+    }
+
+
+
