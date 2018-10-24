17360008acc019755073dc7faa010861570ade3b
HANPASS\055055 <055055@hanpass.com>
2018-10-24 10:02:21 +0900

    20181024 HelloController add, DemoApplication mod

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
+@ComponentScan
 public class DemoApplication {
 
     public static void main(String[] args) {
